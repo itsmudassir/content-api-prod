aeef0c001b1ced03b6d028c4687210659bb92f3b
Muhammad Mudassir <m.mudassir2015@gmail.com>
2022-07-18 19:56:43 +0500

    Update user.Controller.js

--- src/controllers/userControllers/user.Controller.js
+++ src/controllers/userControllers/user.Controller.js
@@ -172,8 +172,8 @@ function setTokenCookie(res, token) {
     const cookieOptions = {
 //         httpOnly: true,
         expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
-        sameSite: 'none',
-        secure: true
+//         sameSite: 'none',
+//         secure: true
         
     };
     res.cookie('refreshToken', token, cookieOptions);
